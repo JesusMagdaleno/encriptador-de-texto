a160554677ec6e04b4c13b83f745f0d5d6a70fa8
Jesus Magdaleno Palma <chuy_chutres@hotmail.com>
2024-08-10 23:49:27 -0600

    Emoezando JS

---
+++ app.js
@@ -0,0 +1,15 @@
+const textarea = document.querySelector(".text-area");
+const mensaje = document.querySelector(",mensaje");
+
+/*
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function encriptar(stringEncriptar){
+    let matrizReglas = [["e", "enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptar = stringEncriptar.tolowercase();
+
+}
